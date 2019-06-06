eba633f3dab819f1b4a35b7093a0faaed3ad3699
thisSyntaxSucksAndYouKnowIt <45504882+thisSyntaxSucksAndYouKnowIt@users.noreply.github.com>
2019-06-06 03:15:06 +0200

    mass like function finished

--- farming.py
+++ farming.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 from actions import *
 import time
 
@@ -54,3 +55,25 @@ def sort_profiles(browser, usr_list):
             clean_list.append(usr)
 
     return clean_list
+
+def mass_like(browser, usr_list, number_of_likes):
+    #//div[contains(@class, 'Nnq7C weEfm')][replace]/div[replace]/a
+
+    row = len(browser.find_elements_by_xpath("//div[contains(@class, 'Nnq7C weEfm')]"))
+    for usr in usr_list:
+        posts = []
+        browser.get(usr)
+        for r in (1, row):
+            for c in (1, 3):
+                try:
+                    posts.append(browser.find_element_by_xpath("//div[contains(@class, 'Nnq7C weEfm')]["+str(r)+"]/div["+str(c)+"]/a").get_attribute("href"))
+                except NoSuchElementException:
+                    break
+
+                if len(posts) == number_of_likes:
+                    break
+
+        for post in posts:
+            browser.get(post)
+            time.sleep(1)
+            like_picture(browser)
