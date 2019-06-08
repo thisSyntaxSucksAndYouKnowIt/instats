bc3bd0853c1718307dbafca89c8850df2ebd5605
thisSyntaxSucksAndYouKnowIt <45504882+thisSyntaxSucksAndYouKnowIt@users.noreply.github.com>
2019-06-08 23:50:06 +0200

    tui menu

--- tui.py
+++ tui.py
@@ -6,3 +6,13 @@ def clear_screen():
         os.system("cls")
     else:
         os.system("clear")
+
+def title_screen():
+    print("")
+    print("  _____           _        _       ")
+    print(" |_   _|         | |      | |      ")
+    print("   | |  _ __  ___| |_ __ _| |_ ___ ")
+    print("   | | | '_ \/ __| __/ _` | __/ __|")
+    print("  _| |_| | | \__ \ || (_| | |_\__ \\")
+    print(" |_____|_| |_|___/\__\__,_|\__|___/")
+    print("")
