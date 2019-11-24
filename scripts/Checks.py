1a055c4347c234e4f35986a2d15b603200dbb910
thisSyntaxSucksAndYouKnowIt <45504882+thisSyntaxSucksAndYouKnowIt@users.noreply.github.com>
2019-11-24 14:25:25 +0100

    fixed browser inheritance

--- scripts/Checks.py
+++ scripts/Checks.py
@@ -10,7 +10,7 @@ class Checks:
 
     def is_empty(self):
         try:
-            WebDriverWait(self.browser, 2).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'v1Nh3')]")))
+            WebDriverWait(self, 2).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'v1Nh3')]")))
         except TimeoutException:
             return True
         return False
@@ -21,28 +21,28 @@ class Checks:
 
     def is_already_liked(self):
         try:
-            liked = WebDriverWait(self.browser, 2).until(EC.presence_of_element_located((By.XPATH, "//span[@aria-label = 'Unlike']")))
+            liked = WebDriverWait(self, 2).until(EC.presence_of_element_located((By.XPATH, "//span[@aria-label = 'Unlike']")))
         except TimeoutException:
             return False
         return True
 
     def is_spam_prevention(self):
         try:
-            WebDriverWait(self.browser, 2).until(EC.presence_of_element_located((By.XPATH, "//p[contains(string(), 'Please wait a few minutes before you try again.')]")))
+            WebDriverWait(self, 2).until(EC.presence_of_element_located((By.XPATH, "//p[contains(string(), 'Please wait a few minutes before you try again.')]")))
         except TimeoutException:
             return False
         return True
 
     def is_username_wrong(self):
         try:
-            WebDriverWait(self.browser, 3).until(EC.presence_of_element_located((By.XPATH, "//p[contains(string(), 'The username you entered')]")))
+            WebDriverWait(self, 3).until(EC.presence_of_element_located((By.XPATH, "//p[contains(string(), 'The username you entered')]")))
         except TimeoutException:
             return False
         return True
 
     def is_password_wrong(self):
         try:
-            WebDriverWait(self.browser, 3).until(EC.presence_of_element_located((By.XPATH, "//p[contains(string(), 'Sorry, your password was incorrect')]")))
+            WebDriverWait(self, 3).until(EC.presence_of_element_located((By.XPATH, "//p[contains(string(), 'Sorry, your password was incorrect')]")))
         except TimeoutException:
             return False
         return False
