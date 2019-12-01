cf75a0af48158040b35efae2f239c573276b80eb
thisSyntaxSucksAndYouKnowIt <45504882+thisSyntaxSucksAndYouKnowIt@users.noreply.github.com>
2019-12-01 16:13:47 +0100

    Fixed mistakes

--- scripts/Realism.py
+++ scripts/Realism.py
@@ -7,23 +7,23 @@ class Realism:
     def __init__(self):
         pass
 
-    def random_number():
+    def random_number(self):
         return random.randint(1, 80) / 295
 
-    def timing_act(min_num, max_num):
+    def timing_act(self, min_num, max_num):
         return random.randrange(min_num, max_num)
 
-    def scroll_down(browser):
+    def scroll_down(self):
         for n in range(0, random.randrange(7,9)):
             time.sleep(random.uniform(0.003, 0.008))
-            browser.execute_script("window.scrollBy(0,"+str(random.uniform(40,45))+")")
+            self.execute_script("window.scrollBy(0,"+str(random.uniform(40,45))+")")
 
-    def scroll_up(browser):
+    def scroll_up(self):
         for n in range(random.randrange(-9,-7), 0):
             time.sleep(random.uniform(0.003, 0.008))
-            browser.execute_script("window.scrollBy(0,"+str(random.uniform(-40,-45))+")")
+            self.execute_script("window.scrollBy(0,"+str(random.uniform(-40,-45))+")")
 
-    def realistic_typing(box, string):
+    def realistic_typing(self, box, string):
         random.seed(datetime.datetime.now())
 
         for char in string:
